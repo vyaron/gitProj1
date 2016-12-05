894130a59f445385490816093034e6d75d1ef37e
Yaron Biton <vyaron@gmail.com>
2016-12-05 11:05:44 +0200

    improved comment

--- getRandomNum.js
+++ getRandomNum.js
@@ -1,4 +1,4 @@
-// Returns a random integer between min (included) and max (excluded)
+// This function Returns a random integer between min (included) and max (excluded)
 // Using Math.round() will give you a non-uniform distribution!
 function getRandomInt(min, max) {
   min = Math.ceil(min);
