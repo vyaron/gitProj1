730cc8596ba2e6d75c4b12dd84f99a3a27e28a2a
Yaron Biton <vyaron@gmail.com>
2016-12-05 11:03:26 +0200

    My Inital Commit

---
+++ getRandomNum.js
@@ -0,0 +1,15 @@
+// Returns a random integer between min (included) and max (excluded)
+// Using Math.round() will give you a non-uniform distribution!
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+// Returns a random integer between min (included) and max (included)
+// Using Math.round() will give you a non-uniform distribution!
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
